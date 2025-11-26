cd1793cb3358b364fd6b4679cf5c212b2cd058aa
kmabhinai03 <129069238+kmabhinai03@users.noreply.github.com>
2025-11-26 11:01:22 +0530

    kothavi

--- server.js
+++ server.js
@@ -105,6 +105,66 @@ const server = http.createServer((req, res) => {
     return handleMovie(req, res, title);
   }
 
+  if (pathname.startsWith("/newMovies") && req.method === "GET") {
+    return res.status(200).json({
+    "title": "Avatar",
+    "year": "2009",
+    "rated": "PG-13",
+    "released": "18 Dec 2009",
+    "runtime": "162 min",
+    "genre": "Action, Adventure, Fantasy",
+    "director": "James Cameron",
+    "writer": "James Cameron",
+    "actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+    "plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+    "language": "English, Spanish",
+    "Country": "USA, UK",
+    "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+    "Metascore": "83",
+    "imdbRating": "7.9",
+    "imdbVotes": "890,617",
+    "imdbID": "tt0499549",
+    "Type": "movie",
+    "Response": "True",
+    "Images": [
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyOTYyMzUxNl5BMl5BanBnXkFtZTcwNTg0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg%22",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BNzM2MDk3MTcyMV5BMl5BanBnXkFtZTcwNjg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg%22",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY2ODQ3NjMyMl5BMl5BanBnXkFtZTcwODg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg%22",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTMxOTEwNDcxN15BMl5BanBnXkFtZTcwOTg0MTUzNA@@._V1_SX1777_CR0,0,1777,999_AL_.jpg%22",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTYxMDg1Nzk1MV5BMl5BanBnXkFtZTcwMDk0MTUzNA@@._V1_SX1500_CR0,0,1500,999_AL_.jpg%22"
+    ]
+  },
+  {
+    "title": "I Am Legend",
+    "year": "2007",
+    "rated": "PG-13",
+    "released": "14 Dec 2007",
+    "runtime": "101 min",
+    "genre": "Drama, Horror, Sci-Fi",
+    "director": "Francis Lawrence",
+    "writer": "Mark Protosevich (screenplay), Akiva Goldsman (screenplay), Richard Matheson (novel), John William Corrington, Joyce Hooper Corrington",
+    "actors": "Will Smith, Alice Braga, Charlie Tahan, Salli Richardson-Whitfield",
+    "plot": "years after a plague kills most of humanity and transforms the rest into monsters, the sole survivor in New York City struggles valiantly to find a cure.",
+    "language": "English",
+    "Country": "USA",
+    "Awards": "9 wins & 21 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMTU4NzMyNDk1OV5BMl5BanBnXkFtZTcwOTEwMzU1MQ@@._V1_SX300.jpg",
+    "Metascore": "65",
+    "imdbRating": "7.2",
+    "imdbVotes": "533,874",
+    "imdbID": "tt0480249",
+    "Type": "movie",
+    "Response": "True",
+    "Images": [
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI0NTI4NjE3NV5BMl5BanBnXkFtZTYwMDA0Nzc4._V1_.jpg%22",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTIwMDg2MDU4M15BMl5BanBnXkFtZTYwMTA0Nzc4._V1_.jpg%22",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc5MDM1OTU5OV5BMl5BanBnXkFtZTYwMjA0Nzc4._V1_.jpg%22",
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTA0MTI2NjMzMzFeQTJeQWpwZ15BbWU2MDMwNDc3OA@@._V1_.jpg%22"
+    ]
+  });
+  }
+
   sendJSON(res, 404, { error: "Not found" });
 });
 
